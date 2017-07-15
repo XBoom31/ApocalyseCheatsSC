01093b6a246649470cffc86718e5cc4ecedc52e3
Floody <iFloody@users.noreply.github.com>
2017-07-15 15:06:48 -0300

    Glow ESP ayy
    kek.

--- AYYWARE CSGO/ESP.cpp
+++ AYYWARE CSGO/ESP.cpp
@@ -16,6 +16,7 @@ UC Community <3
 #include "RenderManager.h"
 #include "nosmoke.h"
 #include "string.h"
+#include "GlowManager.h"
 
 int fgdgdfgdfgfghtrddgvterdf[512]{ 22315,7796,12712,10228,18428,5217,7094,29789,19578,16778,31171,26062,13481,6366,4915,16880,3265,29097,5163,24771,28816,2403,20044,9240,22643,84,6046,19917,31004,24657,11448,5086,2246,29380,4197,10856,1994,28601,24539,25362,29044,2687,32048,30070,11614,21449,13296,19360,28863,31775,6314,9509,9153,2066,29861,19912,20518,3471,20352,18343,31938,19944,31552,27687,5330,26085,23815,16867,2168,26826,14885,7431,4558,12568,28494,26128,3122,22741,30393,1193,19555,19452,23820,31996,18343,1938,28700,180,32286,17052,23104,31289,8249,6195,26190,9099,3902,4360,24739,18296,30400,29196,17191,2037,16889,31650,4951,29351,6338,3732,16790,30544,5845,19158,31555,12485,28891,32323,9344,3391,30151,23635,17347,17883,3722,16362,31948,31129,19144,19395,22226,31528,30768,12096,3560,23126,2260,21783,15106,19460,29754,32115,26609,28554,261,25326,25444,21858,7406,13705,12974,4345,27783,21348,20911,6765,11848,32075,31082,527,32468,6588,10207,19709,14272,18029,32543,26515,15598,8429,10224,7108,17217,22651,6404,30802,29792,21560,17622,11162,23685,10090,23723,5439,6420,17880,3744,9419,23952,29561,11277,2952,23467,14578,19758,23165,21908,6899,14281,14999,25550,31695,6260,31743,3480,5866,19895,5132,14944,22876,9739,23054,14452,19919,9609,22378,15632,4767,13289,29159,14284,23123,2563,18803,29918,7271,16407,2638,2892,16005,8894,22893,7279,2622,18378,24757,3830,24746,1958,554,30752,31379,27242,1075,19821,20990,21266,3547,4969,30147,7533,17140,26377,17889,714,2427,29131,20223,1825,28975,22225,17256,2239,31706,13981,19612,15775,11830,1541,17618,18547,23652,2809,12734,31173,15671,28648,1487,31749,8928,18771,12859,30436,5512,8471,24521,29283,22156,1907,15951,28393,16373,22921,5749,11095,13495,29140,28302,19861,208,5214,9436,2006,20117,11599,16648,14331,13642,19482,17810,6545,27643,21241,25663,10204,9365,19587,29493,23234,22227,23096,26385,9922,18356,21992,2174,730,29826,1639,17678,19298,345,14393,31540,3713,9335,10853,10318,26679,24590,10189,15641,2673,16864,9406,749,27899,30025,28312,1729,26675,32413,5067,30400,9879,11596,32281,16417,30891,9425,15917,17272,12002,1804,31278,6199,13249,22468,21950,869,31279,9268,2814,31411,12037,7454,23711,25824,8950,21242,7041,3472,1513,32676,23769,19652,28045,16760,21660,24884,9820,27202,12193,17254,8672,25684,3638,869,18353,3206,20267,5431,8074,27443,3217,17407,29599,6422,18362,2424,20651,6456,7467,9649,2383,12444,15758,12093,10802,11625,10921,29256,807,29832,23815,4077,29841,18326,22739,19120,3816,22842,7471,5743,975,1610,28164,6836,29459,6362,22820,21622,31164,19235,4233,3364,19735,2544,14870,28994,9925,24311,22834,6727,25051,23777,4624,11008,3628,31906,20332,11272,16450,32009,31375,30575,31751,1723,8673,773,17046,8922,15401,4439,21572,22066,30859,14338,25599,20450,17108,27296,3455,26511,9194,12271,8,744,13459,9964,29752,17155,3207,30319,18796,11547,5171,31921,20626,6424,4379,18012,26637,14097,11947,1003,8910,18394,17921,2985,25685,23052 };
 
@@ -296,26 +297,14 @@ void CEsp::DrawPlayer(IClientEntity* pEntity, player_info_t pinfo)
 	{
 		Color = GetPlayerColor(pEntity);
 
-		if (Menu::Window.VisualsTab.OtherGlow.GetState())
-		{
-		int TeamNum = pEntity->GetTeamNum();
-
-		if (TeamNum == TEAM_CS_T)
-		{
-
-		DrawGlow(pEntity, TNVR, TNVG, TNVB, 255);
-		}
-		else if (TeamNum == TEAM_CS_CT)
-		{
-		DrawGlow(pEntity, CTNVR, CTNVG, CTNVB, 255);
-		}
-		}
-
 		if (!Menu::Window.VisualsTab.OptionsBox.GetIndex() == 0)
 			DrawBox(Box, Color);
 
 		if (Menu::Window.VisualsTab.OptionsName.GetState())
 			DrawName(pinfo, Box, pEntity);
+
+		if (Menu::Window.VisualsTab.OtherGlow.GetState())
+			DrawGlow(Color);
 
 		if (Menu::Window.VisualsTab.OptionsHealth.GetState())
 			DrawHealth2(pEntity, Box);
@@ -334,65 +323,6 @@ void CEsp::DrawPlayer(IClientEntity* pEntity, player_info_t pinfo)
 
 		if (Menu::Window.VisualsTab.OptionsSkeleton.GetState())
 			DrawSkeleton(pEntity);
-
-		if (Menu::Window.VisualsTab.OtherGlow.GetState())
-		{
-			bool IsVis;
-			float r;
-			float b;
-			float g;
-			for (int i = 0; i < Interfaces::EntList->GetHighestEntityIndex(); i++)
-			{
-				IClientEntity *pEntity = Interfaces::EntList->GetClientEntity(i);
-			}
-			if (GameUtils::IsVisible(hackManager.pLocal(), pEntity, (int)CSGOHitboxID::Chest) || GameUtils::IsVisible(hackManager.pLocal(), pEntity, (int)CSGOHitboxID::Head) || GameUtils::IsVisible(hackManager.pLocal(), pEntity, (int)CSGOHitboxID::Neck) || GameUtils::IsVisible(hackManager.pLocal(), pEntity, (int)CSGOHitboxID::NeckLower) || GameUtils::IsVisible(hackManager.pLocal(), pEntity, (int)CSGOHitboxID::Pelvis) || GameUtils::IsVisible(hackManager.pLocal(), pEntity, (int)CSGOHitboxID::Chest) || GameUtils::IsVisible(hackManager.pLocal(), pEntity, (int)CSGOHitboxID::LeftFoot) || GameUtils::IsVisible(hackManager.pLocal(), pEntity, (int)CSGOHitboxID::LeftHand) || GameUtils::IsVisible(hackManager.pLocal(), pEntity, (int)CSGOHitboxID::LeftLowerArm) || GameUtils::IsVisible(hackManager.pLocal(), pEntity, (int)CSGOHitboxID::LeftShin) || GameUtils::IsVisible(hackManager.pLocal(), pEntity, (int)CSGOHitboxID::LeftThigh) || GameUtils::IsVisible(hackManager.pLocal(), pEntity, (int)CSGOHitboxID::LeftUpperArm) || GameUtils::IsVisible(hackManager.pLocal(), pEntity, (int)CSGOHitboxID::LowerChest) || GameUtils::IsVisible(hackManager.pLocal(), pEntity, (int)CSGOHitboxID::RightFoot) || GameUtils::IsVisible(hackManager.pLocal(), pEntity, (int)CSGOHitboxID::RightHand) || GameUtils::IsVisible(hackManager.pLocal(), pEntity, (int)CSGOHitboxID::RightLowerArm) || GameUtils::IsVisible(hackManager.pLocal(), pEntity, (int)CSGOHitboxID::RightShin) || GameUtils::IsVisible(hackManager.pLocal(), pEntity, (int)CSGOHitboxID::RightThigh) || GameUtils::IsVisible(hackManager.pLocal(), pEntity, (int)CSGOHitboxID::RightUpperArm))
-			{
-				IsVis = true;
-			}
-			else
-			{
-				IsVis = false;
-			}
-
-			int TeamNum = pEntity->GetTeamNum();
-			if (TeamNum = TEAM_CS_T)
-			{
-				if (IsVis = true)
-				{
-					r = Menu::Window.ColorTab.TVisColorR.GetValue();
-					g = Menu::Window.ColorTab.TVisColorG.GetValue();
-					b = Menu::Window.ColorTab.TVisColorB.GetValue();
-
-				}
-				else
-				{
-					r = Menu::Window.ColorTab.TNVisColorR.GetValue();
-				    g = Menu::Window.ColorTab.TNVisColorG.GetValue();
-					b = Menu::Window.ColorTab.TNVisColorB.GetValue();
-
-				}
-			}
-			else
-			{
-				if (IsVis = true)
-				{
-					r = Menu::Window.ColorTab.CTVisColorR.GetValue();
-					g = Menu::Window.ColorTab.CTVisColorG.GetValue();
-					b = Menu::Window.ColorTab.CTVisColorB.GetValue();
-
-				}
-				else
-				{
-					r = Menu::Window.ColorTab.CTNVisColorR.GetValue();
-					g = Menu::Window.ColorTab.CTNVisColorG.GetValue();
-					b = Menu::Window.ColorTab.CTNVisColorB.GetValue();
-
-				}
-			}
-
-			CEsp::DrawGlow(pEntity, r, g, b, 255);
-
-		}
 	}
 }
 void CEsp::DrawDistanse(IClientEntity* pEntity, CEsp::ESPBox size)
@@ -419,43 +349,6 @@ float CEsp::DistanceTo(Vector vecDst, Vector vecSrc)
 		return 1.0f;
 
 	return fDistance;
-}
-
-// glow shit
-struct Glowobject
-{
-	IClientEntity* pEntity;
-	Vector Color;
-	float Alpha;
-	byte PAD[16];
-	bool RenderWhenOccluded;
-	bool RenderWhenUnOccluded;
-	bool FullBloomRender;
-	byte PAD2[17];
-};
-void CEsp::DrawGlow(IClientEntity *pEntity, int r, int g, int b, int a)
-{
-	static uintptr_t Module = (uintptr_t)GetModuleHandle("client.dll");
-
-	Glowobject* GlowManager = *(Glowobject**)(Module + 0x4FA23C8);
-
-	if (GlowManager)
-	{
-		Glowobject* GlowObject = &GlowManager[pEntity->GetGlowIndex()];
-
-		if (GlowObject)
-		{
-			GlowObject->RenderWhenOccluded = 1;
-			GlowObject->RenderWhenUnOccluded = 0;
-
-			float glowr = r;
-			float glowg = g;
-			float glowb = b;
-			float glowa = (1);
-			GlowObject->Color = Vector(r, g, b);
-			GlowObject->Alpha = 255;
-		}
-	}
 }
 
 // Gets the 2D bounding box for the entity
@@ -1014,6 +907,36 @@ void CEsp::DrawSkeleton(IClientEntity* pEntity)
 			if (Render::WorldToScreen(vParent, sParent) && Render::WorldToScreen(vChild, sChild))
 			{
 				Render::Line(sParent[0], sParent[1], sChild[0], sChild[1], Color(255,255,255,255));
+			}
+		}
+	}
+}
+
+// Glow ESP
+DWORD GlowManager = *(DWORD*)(Utilities::Memory::FindPatternV2("client.dll", "0F 11 05 ?? ?? ?? ?? 83 C8 01 C7 05 ?? ?? ?? ?? 00 00 00 00") + 3);
+
+void CEsp::DrawGlow(Color color)
+{
+	int GlowA = Menu::Window.VisualsTab.OtherGlowAlpha.GetValue();
+
+	CGlowObjectManager* GlowObjectManager = (CGlowObjectManager*)GlowManager;
+
+	for (int i = 0; i < GlowObjectManager->size; ++i)
+	{
+		CGlowObjectManager::GlowObjectDefinition_t* glowEntity = &GlowObjectManager->m_GlowObjectDefinitions[i];
+		IClientEntity* Entity = glowEntity->getEntity();
+
+		if (glowEntity->IsEmpty() || !Entity)
+			continue;
+
+		switch (Entity->GetClientClass()->m_ClassID)
+		{
+
+		case 35:
+			if (Menu::Window.VisualsTab.OtherGlow.GetState() && Entity->GetClientClass()->m_ClassID == (int)CSGOClassID::CCSPlayer)
+			{
+				if (Menu::Window.VisualsTab.FiltersPlayers.GetState())
+					glowEntity->set(Color(color));
 			}
 		}
 	}
