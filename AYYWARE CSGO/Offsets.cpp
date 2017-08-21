ba7441de504964ba30e445c0d19b860d352bca38
XBoom31 <relubadea333@gmail.com>
2017-08-21 17:49:56 +0300

    uff yaa

--- AYYWARE CSGO/Offsets.cpp
+++ AYYWARE CSGO/Offsets.cpp
@@ -1,5 +1,5 @@
 /*
-Rest In Peace ApocalypseCheats
+Rest In Peace ApocalypseCheats1
 */
 
 #include "Offsets.h"
@@ -86,7 +86,7 @@ void Offsets::Initialise()
 	Functions::KeyValues_KeyValues += 7;
 	Functions::KeyValues_KeyValues = Functions::KeyValues_KeyValues + *reinterpret_cast< PDWORD_PTR >(Functions::KeyValues_KeyValues + 1) + 5;
 
-	Functions::KeyValues_LoadFromBuffer = Utilities::Memory::FindPattern("client.dll", (PBYTE)"\xE8\x00\x00\x00\x00\x8A\xD8\xFF\x15\x00\x00\x00\x00\x84\xDB", "x????xxxx????xx");
+	Functions::KeyValues_LoadFromBuffer = Utilities::Memory::FindPattern("client.dll", (PBYTE)"\xE8\x00\x00\x00\x00\x80\x7D\xF8\x00\x00\x00\x85\xDB", "x????xxxx??xx");
 	Functions::KeyValues_LoadFromBuffer = Functions::KeyValues_LoadFromBuffer + *reinterpret_cast< PDWORD_PTR >(Functions::KeyValues_LoadFromBuffer + 1) + 5;
 
 	Functions::dwCalcPlayerView = Utilities::Memory::FindPattern("client.dll", (PBYTE)"\x84\xC0\x75\x08\x57\x8B\xCE\xE8\x00\x00\x00\x00\x8B\x06", "xxxxxxxx????xx");
